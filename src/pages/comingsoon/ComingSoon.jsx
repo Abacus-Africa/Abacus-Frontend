8aae9652b40192877326d8e8b376922ed5cdf560
damax-codz <kingowu878@gmail.com>
2022-06-26 10:22:16 +0100

    added transition to menu dropdown

--- src/pages/comingsoon/ComingSoon.jsx
+++ src/pages/comingsoon/ComingSoon.jsx
@@ -4,10 +4,9 @@ import {
   faFacebook,
   faLinkedinIn,
   faTwitter,
-  
 } from "@fortawesome/free-brands-svg-icons";
 
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./comingpage.scss";
 import Logo from "../../assets/images/logo.png";
 import MainImg from "../../assets/images/coming-pageimg.png";
@@ -15,17 +14,19 @@ import MainImg from "../../assets/images/coming-pageimg.png";
 import Mailchimp from "../../components/mailchimp/Mailchimp";
 
 function ComingSoon(props) {
-  
-  function dropDown(){
-    let drop = document.querySelector('.drop')
-    if( drop.style.height === '200px' ){
-      drop.style.height ='0px'
-      document.querySelectorAll('.droplink').forEach((item)=>item.style.display='none')
-    }
-    else{
-      drop.style.height ='200px'
+  function dropDown() {
+    let drop = document.querySelector(".drop");
+    if (drop.style.height === "200px") {
+      drop.style.height = "0px";
+      document
+        .querySelectorAll(".droplink")
+        .forEach((item) => (item.style.display = "none"));
+    } else {
+      drop.style.height = "200px";
       setTimeout(() => {
-        document.querySelectorAll('.droplink').forEach((item)=>item.style.display='flex')
+        document
+          .querySelectorAll(".droplink")
+          .forEach((item) => (item.style.display = "flex"));
       }, 650);
     }
   }
@@ -34,11 +35,11 @@ function ComingSoon(props) {
     <div className="flex box-border">
       <div className="w-9/12 left-side h-screen">
         <header className="flex justify-between mt-12 mx-44 headings">
-
           <div>
-            <a href="/"><img src={Logo} alt="abacus logo"></img></a>
+            <a href="/">
+              <img src={Logo} alt="abacus logo"></img>
+            </a>
           </div>
-
 
           <div className="links flex">
             <nav className="flex justify-end space-x-4 ">
@@ -50,8 +51,11 @@ function ComingSoon(props) {
                 <a
                   href={url}
                   //when active class is added to the link, the color of the link changes to red
-                  className={`${window.location.pathname === url ? " text-pviolet linkStyles  " : " linkStyles "}`}
-                  
+                  className={`${
+                    window.location.pathname === url
+                      ? " text-pviolet linkStyles  "
+                      : " linkStyles "
+                  }`}
                 >
                   {title}
                 </a>
@@ -60,23 +64,31 @@ function ComingSoon(props) {
           </div>
 
           <div className="menu-dropdown" onClick={dropDown}>
-          <FontAwesomeIcon icon={faBars} className=" menu-button ml-6" />
+            <FontAwesomeIcon icon={faBars} className=" menu-button ml-6" />
           </div>
-
         </header>
 
         {/* dropDown Nav */}
         <div className="nav-dropdown">
-        <nav className="flex flex-col items-end mt-5 mr-3 drop">
-              {[
-                ["Home", "/"],
-                ["FAQ", "/faq"],
-                ["Blog", "https://abacusafrica.hashnode.dev/"],
-              ].map(([title, url]) => (
-                <a href={url} className={`${window.location.pathname === url ? " text-pviolet linkStyles droplink" : " linkStyles droplink"}`}>{title}</a>
-                ))}
-            </nav>
-          </div>
+          <nav className="flex flex-col items-end mt-5 mr-3 drop">
+            {[
+              ["Home", "/"],
+              ["FAQ", "/faq"],
+              ["Blog", "https://abacusafrica.hashnode.dev/"],
+            ].map(([title, url]) => (
+              <a
+                href={url}
+                className={`${
+                  window.location.pathname === url
+                    ? " text-pviolet linkStyles droplink"
+                    : " linkStyles droplink"
+                }`}
+              >
+                {title}
+              </a>
+            ))}
+          </nav>
+        </div>
 
         <main className="px-52 text-center align-center my-28 box-border gen-text">
           <p className="text-3xl mb-12 abacu_s">
@@ -84,28 +96,41 @@ function ComingSoon(props) {
             Software to manage your business
           </p>
           <div className="comingimg">
-          <img src={MainImg} alt="coming soon" ></img>
+            <img src={MainImg} alt="coming soon"></img>
           </div>
           <h1 className="pt-2 leading-snug font-medium text-7xl mb-10 com_ing">
             Coming Soon
           </h1>
-          <p className='launch text-lg'>Be the first to know when we launch</p>
-          
-            <Mailchimp />
+          <p className="launch text-lg">Be the first to know when we launch</p>
 
+          <Mailchimp />
         </main>
 
         <footer className="ml-40 mt-8 overflow-hidden footer">
           <div>
-          <h2 className="text-3xl font-bold">Abacus</h2>
-          <p className="text-xs">As easy as it gets</p>
+            <h2 className="text-3xl font-bold">Abacus</h2>
+            <p className="text-xs">As easy as it gets</p>
           </div>
 
           <div>
-            <a href='https://twitter.com/AbacusAfrica' target='blank'><FontAwesomeIcon icon={faTwitter} className="mr-6" /></a>
-            <a href='https://www.facebook.com/AbacusAfrica?_rdc=1&_rdr' target='blank'><FontAwesomeIcon icon={faFacebook} className="mr-6" /></a>
-            <a href='https://www.linkedin.com/company/abacus-africa/' target='blank'><FontAwesomeIcon icon={faLinkedinIn} className="mr-6" /></a>
-            <a href='https://www.instagram.com/abacusafrica/' target='blank'><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://twitter.com/AbacusAfrica" target="blank">
+              <FontAwesomeIcon icon={faTwitter} className="mr-6" />
+            </a>
+            <a
+              href="https://www.facebook.com/AbacusAfrica?_rdc=1&_rdr"
+              target="blank"
+            >
+              <FontAwesomeIcon icon={faFacebook} className="mr-6" />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/abacus-africa/"
+              target="blank"
+            >
+              <FontAwesomeIcon icon={faLinkedinIn} className="mr-6" />
+            </a>
+            <a href="https://www.instagram.com/abacusafrica/" target="blank">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
           </div>
         </footer>
       </div>
