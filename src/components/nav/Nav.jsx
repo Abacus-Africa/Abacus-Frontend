118a96fe67ecf92288227b2a73084d806cd38dae
Ruby <graceffiong@gmail.com>
2022-07-07 22:27:51 +0100

    deploy errors removed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { Box, Dialog, IconButton, Slide } from "@mui/material";
+import { Box, IconButton} from "@mui/material";
 import React from "react";
 import Logo from "../../assets/images/logo.png";
 import "./Nav.css";
